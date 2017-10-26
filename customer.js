7c41e1e80a476f06a90c1a963f1c93c8a921bf15
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-10-25 23:15:36 -0400

    Installed CLI-table node package module, made progress on customer.js

--- customer.js
+++ customer.js
@@ -1,7 +1,7 @@
 // Declaring npm dependencies
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+var table = require("cli-table");
 // Establish connection to MySQL database
 var connection = mysql.createConnection({
 	host: "Local_Host",
@@ -35,6 +35,48 @@ var startBamazon = function() {
 			]
 		})
 		.then(function (answer){
-			
+			switch(answer.action){
+
+				case "Browse Bamazon Shop";
+					showInventory();
+					break;
+
+				case "Purchase Products";
+					buyProducts();
+					break;
+
+				case "Low-Stock Inventory";
+					lowInventory();
+					break;
+
+				case "Quit Bamazon";
+					connection.end();
+					break;
+			}
 		});
 }
+// Below lies the functionality necessary to run the above code!
+// User activates the functions after answering the inquirer prompts...
+
+var showInventory = function() {
+	// Reveals all items in Bamazon database.
+	connection.query("SELECT * FROM products", function (err, res){
+
+		if (err) throw err;
+		// If no error proceed to show inventory...
+		// Arrange inventory in CLI table
+		var cliTable = new table({
+			// Establish column heading
+			head: ["Item ID", "Product Name", "Department", "Price", "In Stock"],
+			colWidths: [15, 25, 20, 15, 15]
+		});
+		// Loop through responses to Parse the information 
+		for (var i = 0; i < res.length; i++) {
+			table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+		}
+		// Display results in Command Line
+		console.log(table.toString());
+		startBamazon();
+	});
+}
+// Begin "buyProducts" function
