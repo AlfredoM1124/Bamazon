fa0bbc287f35ce9404f113e36940837dcddd7c3b
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-10-25 22:26:25 -0400

    Adding inquirer prompts to customer.js

--- customer.js
+++ customer.js
@@ -1,13 +1,40 @@
 // Declaring npm dependencies
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-// Constructor that defines customer's accessibility options 
-var customer = function (username) {
 
-	this.username = username;
-	this.totalPrice = "";
-	var itemCart = [];
-	var options = [];
+// Establish connection to MySQL database
+var connection = mysql.createConnection({
+	host: "Local_Host",
+	port: 3306,
 
-	var 
+	user: "root",
+	password: "",
+	database: "bamazon"
+});
+
+connection.connect(function (err){
+	if(err) throw err;
+	console.log("Connected as ID: " + connection.threadId + "\n");
+	// Initiates the application
+	startBamazon();
+});
+
+var startBamazon = function() {
+	// Prompts user for commands 
+	inquirer
+		.prompt({
+			name: "action",
+			type: "rawlist",
+			message: "Welcome to Bamazon! How would you like to proceed?",
+			// You may choose from one of the following options...
+			choices: [
+			"Browse Bamazon Shop",
+			"Purchase Products",
+			"Low-Stock Inventory",
+			"Quit Bamazon"
+			]
+		})
+		.then(function (answer){
+			
+		});
 }
